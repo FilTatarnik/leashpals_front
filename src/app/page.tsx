a402e594da0d8f56b0e59ff0ed48462dbbf7b1a2
Filip Tatarnik <ftatarnik@gmail.com>
2025-01-02 19:40:49 -0600

    Added appointments to the screen, slowly building the functionality of the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,9 +25,24 @@ async function fetchDogs() {
     return []; // Return an empty array as a fallback
   }
 }
+
+async function fetchAppointments() {
+  try {
+    const res = await fetch('http://localhost:42069/appointments'); 
+    if (!res.ok) {
+      throw new Error(`Failed to fetch: ${res.status}`);
+    }
+    return res.json();
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return []; // Return an empty array as a fallback
+  }
+}
+
 export default async function Home() {
   const users = await fetchUsers();
   const dogs = await fetchDogs();
+  const appointments = await fetchAppointments();
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -46,6 +61,14 @@ export default async function Home() {
           <ul>
             {dogs.map((dog) => (
               <li key={dog.id}>{dog.name}</li>
+            ))}
+          </ul>
+        </div>
+        <div>  
+          <h2>Appointments</h2>
+          <ul>
+            {appointments.map((appointment) => (
+              <li key={appointment.id}>Dog: #{appointment.dog_id} Walker: #{appointment.walker_id} Done: </li>
             ))}
           </ul>
         </div>      
