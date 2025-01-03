fdd9482f9dd415865a05d5413f0d42f8f85bd0b5
Filip Tatarnik <ftatarnik@gmail.com>
2025-01-02 20:53:31 -0600

    chaged the CSS up a bit in the global.css and in tailwind. Started to add a Login Form, Username/Password - and two buttons; Register/Login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Form from 'next/form';
 
 async function fetchUsers() {
   try {
@@ -47,9 +48,15 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>LeashPals</h1>
+        <h1 className="text-6xl">LeashPals</h1>
+        <Form action=''>
+            <input name='query' placeholder="Username" type="text" className="m-2 p-1 text-black"/>
+            <input name='query' placeholder="Password" type="password" className="m-2 p-1 text-black"/>
+            <button type='submit' className="border-2 border-white-500 p-1">Register</button>
+            <button type='submit' className="border-2 border-white-500 p-1 ml-2">Login</button>
+        </Form>        
         <div>
-          <h2>Users</h2>
+          <h2 className='text-2xl'>Users</h2>
           <ul>
             {users.map((user) => (
               <li key={user.id}>{user.username}</li>
@@ -57,7 +64,7 @@ export default async function Home() {
           </ul>
         </div>
         <div>  
-          <h2>Dogs</h2>
+          <h2 className='text-2xl'>Dogs</h2>
           <ul>
             {dogs.map((dog) => (
               <li key={dog.id}>{dog.name}</li>
@@ -65,13 +72,13 @@ export default async function Home() {
           </ul>
         </div>
         <div>  
-          <h2>Appointments</h2>
+          <h2 className='text-2xl'>Appointments</h2>
           <ul>
             {appointments.map((appointment) => (
               <li key={appointment.id}>Dog: #{appointment.dog_id} Walker: #{appointment.walker_id} Done: </li>
             ))}
           </ul>
-        </div>      
+        </div>     
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       </footer>
